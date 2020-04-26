12d33db587e8ad07102667bf9c565bd468039ed4
Jeff <jeff1hu@gmail.com>
2020-04-26 17:18:48 -0600

    dohwa

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 const Discord = require('discord.js')
+const client = new Discord.Client()
+
+// bot startup
+client.on('ready', () => {
+    console.log('ready')
+
+})
+
+// command handler
+client.on('message', message => {
+
+})
+
+
 
