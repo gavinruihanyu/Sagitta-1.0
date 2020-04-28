226b60e09100d8fcd1b220b211b106e043ce9432
Jeff <jeff1hu@gmail.com>
2020-04-28 07:46:02 -0600

    Delete unnecassary whitespace. Correct event order

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-//bot startup sequence:
-
-//requires discord.js from discord.js files
 const Discord = require('discord.js')
+const {prefix, version, token} = require('./ini.js')
 
-//locates and sets prefix, version and token
-const {prefix , version, token} = require('./ini.js')
-
-//creates Sagitta bot
 const client = new Discord.Client()
 
 // bot startup
@@ -14,17 +8,14 @@ client.on('ready', () => {
     client.user.setStatus("online");
     console.log('Sagitta is ready and online, awaiting ');
 })
-//bot login
-client.login(token).then(r => {
-    console.log('Sagitta is logged in.');
-});
 
 // command handler
 client.on('message', message => {
     if (message.author.bot || message.channel.type === 'dm' || !message.content.startsWith(prefix)) return;
+
 })
 
-//TODO: refrence help.js thingy and fix ini.js locator
 
-
-
+client.login(token).then(r => {
+    console.log('Sagitta is logged in.');
+});
