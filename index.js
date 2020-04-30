ac3b97a801e0df4bea15e61d2febc1be88ad153d
Gavin Yu <gxvinyu@gmail.com>
2020-04-29 23:38:53 -0600

    Gavins useless edits

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ let errors = []
 
 // bot startup
 client.on('ready', () => {
-    client.user.setPresence({ activity: { name: 'games' }, status: 'online' })
-    console.log('Sagitta is ready and online, awaiting orders...');
+    client.user.setPresence({ activity: { name: '|Help for commands' }, status: 'online' })
+    console.log('Sagitta '+version+' is ready and online, awaiting orders...');
 })
 //bot login
 client.login(token).then(r => {
@@ -32,13 +32,17 @@ client.on('guildMemberRemove', member => {
 
 // message event
 client.on('message', message => {
+
+    const isBotOwner1 = message.author.id == '186107166686576641';
+    const isBotOwner2 = message.author.id == '312380733668851712';
+
     if (message.author.bot || message.channel.type === 'dm') return;
 
     if (message.content.startsWith(prefix)) {
         const args = message.content.slice(prefix.length).split(/ +/)
         const command = args.shift().toLowerCase()
 
-        if (!client.commands.has(command)) {
+        if (!client.commands.has(command) && message.content!='|!|SagittaShutdown') {
             message.reply('Type |help for the list of commands. fag')
             return
         }
@@ -50,6 +54,25 @@ client.on('message', message => {
             console.log(error)
         }
     }
+    if (message.content=='|!|SagittaShutdown') {
+        if (!isBotOwner1 && !isBotOwner2){
+            message.reply('You must have some nerve to think your little fairy homosexual loving waste of human flesh ass can have the authority to shut me down.')
+            return;
+        }
+        else{
+            const shutdownDialogue=new Discord.MessageEmbed()
+                .setColor(0x770000)
+                .setTitle('Sagitta '+version+' is now deactivating.');
+            message.author.send(shutdownDialogue).then(shutdownSeq => {
+                client.destroy();
+                client.destroy();
+            })
+            console.log('Sagitta has been shut down.')
+        }
+    }
 })
+
+
+
 
 
